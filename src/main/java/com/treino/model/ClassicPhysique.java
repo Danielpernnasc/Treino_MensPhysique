1ca65fa55b4fc79792d279a8ff27120de969d0fc
Danielpernnasc <daniel.nascimento@smartezy.com.br>
2025-10-13 11:32:42 -0300

    atualizando

--- src/main/java/com/treino/model/ClassicPhysique.java
+++
@@ -1,7 +0,0 @@
-package com.treino.model;
-
-
-private Map<String, List<Exercicios>> treinos = new HashMap<>();
-public class ClassicPhysique {
-
-}
--- src/main/java/com/treino/model/Treinos.java
+++
@@ -1,6 +0,0 @@
-package com.treino.model;
-import java.util.List;
-
-public interface Treinos {
-    List<Exercicios> treinoDoDia(String dia);
-}
